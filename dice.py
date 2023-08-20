269b550e89e51e0de8e64d76f51e9e87637b7075
sharanya-singh <114478756+sharanya-singh@users.noreply.github.com>
2023-08-20 20:15:33 +0530

    Update dice.py

--- dice.py
+++ dice.py
@@ -3,13 +3,12 @@ import random
 print("🎲🎲INFINITY DICE🎲🎲")
 
 def infi_dice():
-  while True:
-    play= input("Do you want to roll the dice?")
-    u_side=int(input("Enter number of sides that you want your dice to have\n---->\n"))
-    if play=="YES" or "yes" or "Yes":
-      print("You rolled",random.randint(1,u_side))
-    else :
-        print("k buddy. Bye then")
-
+   if play=="yes":
+         u_side=int(input("Enter number of sides that you want your dice to have\n---->\n"))
+         print("You rolled",random.randint(1,u_side))
+   elif play=="no":
+         print("k buddy. Bye then")
+play= input("Do you want to roll the dice?\n")
 infi_dice()
-
+play=input("Do you want to roll again?\n")
+infi_dice()
